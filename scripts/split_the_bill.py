1d839b529e054d38ea995d650117ded510a65ddf
Appendix <1223241043@qq.com>
2025-07-08 16:56:27 +0800

    可执行程序构建

--- scripts/split_the_bill.py
+++ scripts/split_the_bill.py
@@ -11,9 +11,13 @@ class Spliter:
         self.average_cost = 0
         self.algo = DEFAULT_ALGO
         self.payments = {}
+        set_output('欢迎使用自助分账系统。')
 
+    # 增加一个人的数据
     def add_person(self):
-        name = get_input('\n请输入姓名：')
+        name = ''
+        while name == '':
+            name = get_input('请输入姓名：')
         if name not in self.people:
             accounts = []
             weight = 1.0
@@ -28,18 +32,28 @@ class Spliter:
                 weight = float(input_weight)
             self.people[name] = {'accounts':accounts,'weight':weight}
 
+    # 计算
     def calculate(self):
-        self.total = sum([sum(person['accounts']) for person in self.people.values()])
-        self.average_cost = round(self.total / len(self.people),2)
-        total_weight = sum([p['weight'] for p in self.people.values()])
+        self.total = round(sum([sum(person['accounts']) for person in self.people.values()]),2)  # 总金额
+        self.average_cost = round(self.total / len(self.people),2)      # 人均金额，未使用
+        total_weight = sum([p['weight'] for p in self.people.values()])     # 权重总和，作为个人权重的分母
         for person in self.people.values():
-            weight = person['weight'] / total_weight
-            payable_amount = self.total * weight
-            balance = payable_amount - sum(person['accounts'])
+            weight = person['weight'] / total_weight    # 个人权重
+            payable_amount = self.total * weight    # 理论需支付金额
+            balance = payable_amount - sum(person['accounts'])  # 减去已支付金额后的实际结余
             payable_amount,balance = round(payable_amount,2),round(balance,2)
             person['payable_amount'] = payable_amount
             person['balance'] = balance
         self.payments = self.algo.run(self.people)
+    
+    # 展示明细，如总金额
+    def show_detail(self):
+        set_output('\n总金额{}元'.format(spliter.total))
+        for name,person in self.people.items():
+            set_output('{}需支付{}元'.format(name,person['payable_amount']))
+        set_output('\n转账方案：')
+        for payment in self.payments:
+            set_output(payment)
 
 class Payment:
     def __init__(self,from_who:str,to_who:str,amount:float):
@@ -68,8 +82,13 @@ def set_output(message):
     print(message)
 
 if __name__ == "__main__":
-    spliter = Spliter(DATA)
+    spliter = Spliter()
+    n = int(get_input('请输入总人数：'))
+    if n > 0:
+        for i in range(n):
+            set_output('\n第{}人'.format(i+1))
+            spliter.add_person()
     spliter.calculate()
-    print(sum([p['payable_amount'] for p in spliter.people.values()]),spliter.total)
-    for payment in spliter.payments:
-        set_output(payment)
+    # print(sum([p['payable_amount'] for p in spliter.people.values()]),spliter.total)
+    spliter.show_detail()
+    input('按回车键退出')
